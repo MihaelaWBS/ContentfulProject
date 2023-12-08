4ec070db6507e61afbac4fcaa37dd88c47ed00d3
MihaelaWBS <mihaela.mocanu.ux@gmail.com>
2023-12-08 08:49:05 +0100

    I added some content in the video component and modified the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { createClient } from "contentful";
-import { Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Link, Routes, Route } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Card, Row, Button, Col } from "react-bootstrap";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Video from "./component/Video";
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const navigate = useNavigate(); 
+	
 
 	useEffect(() => {
 		const SPACE_ID = import.meta.env.VITE_SPACE_ID;
@@ -32,40 +35,53 @@ function App() {
 			.catch(console.error);
 	}, []);
 
+	const goToTrailer = (videoUrl) => {
+        navigate('/video', { state: { videoUrl } }); // Navigate to Video component with video URL
+    };
+
+
 	return (
-		<>
-			<Row>
-				{movies.map((movie) => (
-				
-					<Col xs={12} sm={6} md={4} lg={3} className="mb-4">
-					<Card key={movie.sys.id} className="h-100 d-flex flex-column">
-					<Card.Img
-						style={{ height: "300px" }}
-						variant="top"
-						src={movie.fields.image?.fields.file.url}
-					/>
-					<Card.Body className="flex-grow-1 d-flex flex-column">
-						<Card.Title className="mb-3 fw-bold">{movie.fields.title}</Card.Title>
-						<Card.Text className="text-start flex-grow-1">
-						{documentToReactComponents(movie.fields.body)}
-						</Card.Text>
-						{/* Buttons aligned at the bottom */}
-						<div className="mt-auto">
-						<div className="d-flex justify-content-around"> {/* Adjust button spacing */}
-							<Button variant="primary" className="text-nowrap me-2">
-							Details
-							</Button>
-							<Button variant="success" className="text-nowrap">
-							Watch trailer
-							</Button>
-						</div>
-						</div>
-					</Card.Body>
-					</Card>
-				</Col>
-				))}
-			</Row>
-		</>
+		<Router>
+            <Routes>
+                <Route path="/" element={
+
+					<>
+						<Row>
+							{movies.map((movie) => (
+							
+								<Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+								<Card key={movie.sys.id} className="h-100 d-flex flex-column">
+								<Card.Img
+									style={{ height: "300px" }}
+									variant="top"
+									src={movie.fields.image?.fields.file.url}
+								/>
+								<Card.Body className="flex-grow-1 d-flex flex-column">
+									<Card.Title className="mb-3 fw-bold">{movie.fields.title}</Card.Title>
+									<Card.Text className="text-start flex-grow-1">
+									{documentToReactComponents(movie.fields.body)}
+									</Card.Text>
+									{/* Buttons aligned at the bottom */}
+									<div className="mt-auto">
+									<div className="d-flex justify-content-around"> {/* Adjust button spacing */}
+										<Button variant="primary" className="text-nowrap me-2">
+										Details
+										</Button>
+										<Button variant="success" className="text-nowrap" onClick={() => goToTrailer(movie.fields.video?.fields.file.url)}>
+										Watch trailer
+										</Button>
+									</div>
+									</div>
+								</Card.Body>
+								</Card>
+							</Col>
+							))}
+						</Row>
+					</>
+			}/>
+			<Route path="/video" element={<Video />} /> {/* New Route for Video component */}
+		</Routes>
+	</Router>
 
 				
 	);
--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from "react-router-dom"; // Import useLocation
+import { Container, Row, Col, ResponsiveEmbed } from 'react-bootstrap'; // Import Bootstrap components
 
 const Video = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    const location = useLocation();
+    const { videoUrl } = location.state; // Extracting video URL from location state
+
+    return (
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <ResponsiveEmbed aspectRatio="16by9">
+                        <iframe title="Movie Trailer" src={videoUrl} allowFullScreen></iframe>
+                        {/* Embed the video iframe */}
+                    </ResponsiveEmbed>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-export default Video
+export default Video;
